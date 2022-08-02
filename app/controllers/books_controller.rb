3e36f42eef90cfb00f2892ef87f34435cdc05e29
yehia-khalil <yehiakhalilykk@gmail.com>
2022-08-02 15:54:49 +0200

    Added table css

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
     
     def create
         @book = Book.new(book_params)
+        @book.user_id=current_user.id
         if @book.save
           redirect_to root_path
         else
