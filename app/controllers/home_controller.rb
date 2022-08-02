517a9b8acee2b91f16423b1e8983bd499c6bc5b2
yehia-khalil <yehiakhalilykk@gmail.com>
2022-08-02 16:17:39 +0200

    redirect to login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
     def index
         if current_user
             redirect_to books_path
+        else 
+            redirect_to new_user_session_path
         end
-        
     end
 end
